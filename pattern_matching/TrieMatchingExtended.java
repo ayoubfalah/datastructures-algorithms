e35ea4d886c8596f6769f815191c515e5aaf0439
Ayoub Falah <ayoub_falah@hotmail.de>
2016-11-20 19:56:10 +0100

    Extending the TrieMatching algorithm to handle cases when one of the patterns is a prefix of another pattern

---
+++ pattern_matching/TrieMatchingExtended.java
@@ -0,0 +1,168 @@
+import java.io.*;
+import java.util.*;
+
+class TrieMatchingExtended implements Runnable
+{
+    private static final int  INVALID_SHIFT       = -1;
+    private static final int  SUBTEXT_BEGIN_INDEX = 1;
+    private static final char WORD_END_INDICATOR  = '$';
+    
+    /**
+     * 
+     * @param text a reference Text
+     * @param n The number of elements in patterns
+     * @param patterns a List of patterns that will be matched against the
+     *                 reference Text text
+     * @precondition 1 <= |text| <= 10000
+     *               1 <= n <= 5000
+     *               for each pattern in patterns 1<= |pattern| <= 100
+     *               for each c_1 in text, c_2 in pattern:
+     *                        c_1, c_2 in {A, C, G, T}
+     * @return All valid shifts with which a pattern in patterns occurs in 
+     *         text
+     */
+    List <Integer> solve (String text, int n, List <String> patterns) 
+    {
+
+//        List<Map<Character, Integer>> trie = (new HashTableTrie()).
+//                buildTrie(patterns.toArray(new String[patterns.size()]));
+        
+        List<Map<Character, Integer>> trie = buildTrie(patterns
+                .toArray(new String[patterns.size()]));
+
+        List <Integer> result = trieMatching(text, trie);
+
+        return result;
+    }
+
+    public void run () 
+    {
+        try {
+                BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+                String text = in.readLine ();
+                int n = Integer.parseInt (in.readLine ());
+                List <String> patterns = new ArrayList();
+                for (int i = 0; i < n; i++) {
+                    String pattern = in.readLine () + WORD_END_INDICATOR;
+                    patterns.add (pattern);
+                }
+
+                List <Integer> ans = solve (text, n, patterns);
+
+                for (int j = 0; j < ans.size (); j++) {
+                        System.out.print ("" + ans.get (j));
+                        System.out.print (j + 1 < ans.size () ? " " : "\n");
+                }
+        }
+        catch (Throwable e) {
+                e.printStackTrace ();
+                System.exit (1);
+        }
+    }
+
+    public static void main (String [] args) {
+            new Thread (new TrieMatchingExtended()).start ();
+    }
+    
+    public List<Map<Character, Integer>> buildTrie(String[] patterns) 
+    {
+        List<Map<Character, Integer>> adj = new ArrayList();
+        
+        Map<Character, Integer> root = new HashMap<>();
+        char currentSymbol = patterns[0].charAt(0);
+        int newNodeIndex = 0;
+        root.put(currentSymbol, ++newNodeIndex);
+        adj.add(root);
+        adj.add(new HashMap<>());
+        
+        for (String pattern : patterns) 
+        {
+            int currentNodeIndex = 0;
+            Map<Character, Integer> currentNode = adj.get(currentNodeIndex);
+            for (int i = 0; i < pattern.length(); i++) 
+            {
+                currentSymbol = pattern.charAt(i);
+                if (currentNode.containsKey(currentSymbol)) 
+                {
+                    currentNodeIndex = currentNode.get(currentSymbol);
+                    currentNode = adj.get(currentNodeIndex);
+                }else
+                {
+                    Map newNode = currentNode;
+                    newNode.put(currentSymbol, ++newNodeIndex);
+                    adj.add(new HashMap<>());
+                    currentNode = adj.get(newNodeIndex);
+                }            
+            }            
+        }
+
+        return adj;
+    }
+
+    private List<Integer> trieMatching(String text, List<Map<Character, Integer>> trie)
+    {
+        int wholeTextlength = text.length();
+        List <Integer> result = new ArrayList();
+        while (!(text.isEmpty()))
+        {
+            int subResult = prefixTrieMatching(text, trie);
+            boolean isValidShift = subResult >= 0; 
+            if (isValidShift) 
+            {
+                int validShift = wholeTextlength - subResult;                
+                result.add(validShift);                
+            }
+            text = removeFirstSymbol(text);
+        }
+        return result;
+    }
+
+    private String removeFirstSymbol(String text) 
+    {
+        text = text.substring(SUBTEXT_BEGIN_INDEX);
+        return text;
+    }
+
+    private int prefixTrieMatching(String text, List<Map<Character, Integer>> trie)
+    {
+        int currentTextSymbolIndex = 0;
+        char currentSymbol = text.charAt(currentTextSymbolIndex);
+        int rootIndex = 0;
+        Map<Character, Integer> currentNode = trie.get(rootIndex);
+
+        while (true)
+        {
+            // The currentNode is a leaf in trie
+            boolean isCurrentNodeALeaf = currentNode.isEmpty();
+            boolean patternEnd = currentNode.containsKey(WORD_END_INDICATOR);
+            // Is there an edge in trie labeled by current symbol
+            boolean edgeWithCurrentSymbol = currentNode.containsKey(currentSymbol);
+            if (isCurrentNodeALeaf || patternEnd)
+            {
+                return text.length();
+            }
+            else if (edgeWithCurrentSymbol) 
+            {
+                Integer currentNodeIndex = currentNode.get(currentSymbol);
+                currentNode = trie.get(currentNodeIndex);
+                isCurrentNodeALeaf = currentNode.isEmpty();
+                patternEnd = currentNode.containsKey(WORD_END_INDICATOR);
+                if (isCurrentNodeALeaf || patternEnd)
+                {
+                    return text.length();
+                }
+                ++currentTextSymbolIndex;
+                boolean hasNextLetter = currentTextSymbolIndex < text.length();
+                if (hasNextLetter)
+                    currentSymbol = text.charAt(currentTextSymbolIndex);
+                else
+                {
+                    // The length of pattern is larger than the length of
+                    // the text
+                    return INVALID_SHIFT;
+                }                    
+            }
+            else return INVALID_SHIFT;            
+        }
+    }
+}
